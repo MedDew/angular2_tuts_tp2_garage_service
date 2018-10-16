a81426efa283c6edc96f8a5e1c4de34aa01f30f9
Med <mehdi.gacem@versusmind.eu>
2018-10-16 10:43:18 +0200

    Application module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarsModule } from './cars/cars.module';
+import { CarService } from './car/car.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarsModule
   ],
-  providers: [],
+  providers: [CarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
